b84d0cf70c38440513f355fdfc9e3a7b560cd04e
javier1nc <code@javier1nc.com>
2023-06-24 22:09:14 -0600

    sync: 14 Primer observable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,30 @@
 <script lang='ts'>
-const nombre: string = 'Javier';
- console.log(nombre);
+
+import { Observable } from 'rxjs';
+import { subscribe } from 'svelte/internal';
+
+//const obs$ = Observable.create();
+const obs$ = new Observable<string>( subs => {
+	subs.next('Hola');
+	subs.next('Mundo');
+	
+	subs.next('Hola');
+	subs.next('Mundo');
+
+	subs.complete();
+
+	subs.next('Hola');
+	subs.next('Mundo');
+	
+});
+
+obs$.subscribe(console.log);
+
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
 		<h1 class="h1">Curso RxJS</h1>
 		<p>...</p>
-
 	</div>
 </div>
