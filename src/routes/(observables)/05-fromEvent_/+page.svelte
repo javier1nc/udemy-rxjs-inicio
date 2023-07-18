e0fc1b802608ec287c53de4e03f405149c8f7f98
javier1nc <code@javier1nc.com>
2023-07-17 21:40:33 -0600

    sync: 07-intreval-timer

---
+++ src/routes/(observables)/05-fromEvent_/+page.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+
+import { fromEvent } from 'rxjs';
+
+/**
+* Eventos del DOM
+*/
+// BUG!
+const src1$ = fromEvent(document, 'click');
+cosole.log(src1);
+
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Curso RxJS</h1>
+		<p>...</p>
+	</div>
+</div>
---
+++ src/routes/(observables)/06-range/+page.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+
+import { range, asyncScheduler } from 'rxjs';
+
+//const src$ = of(1,2,3,4,5);
+const src$ = range(1,5, asyncScheduler);
+
+console.log("inicio");
+src$.subscribe( console.log );
+console.log("fin");
+
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Curso RxJS</h1>
+		<p>...</p>
+	</div>
+</div>
